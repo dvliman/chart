ee0550d441d249281dba788378ae6e984ffb675c
David Liman <dliman@onescreen.com>
2012-03-14 13:09:02 -0700

    data view causing problem. remove 1 column from data table

--- server.rb
+++ server.rb
@@ -23,11 +23,11 @@ stub = <<eos
 {
   "accountID": "123",
   "count": "4", 
-  "dataPoints": [ {"time": "1328072400", "hits":2, "type":"month"},
-                  {"time": "1328115600", "hits":3, "type":"month"}, 
-                  {"time": "1328119200", "hits":4, "type":"day"},
-                  {"time": "1328119200", "hits":1, "type":"day"},
-                  {"time": "1328072300", "hits":12, "type":"month"}]
+  "dataPoints": [ {"time": "1328072400", "hits":2, "type":"Month"},
+                  {"time": "1328115600", "hits":3, "type":"Month"}, 
+                  {"time": "1328119200", "hits":4, "type":"Day"},
+                  {"time": "1328119200", "hits":1, "type":"Day"},
+                  {"time": "1328072300", "hits":12, "type":"Month"}]
 }
 eos
 
